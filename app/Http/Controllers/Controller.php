1610305e421210b48b14fd7f85ae9b47ae2dce4d
HCLucian <lucian@hospitalcompare.co.uk>
2019-07-02 12:03:26 +0100

    - Created `dev` branch

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     function test($id = 0 ) {
-        //LOGIC
+        //LOGIC HERE
         $variable = [];
         //RETURN
         return compact($variable, 'welcome' );
