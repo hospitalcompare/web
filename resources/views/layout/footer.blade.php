4fbbf60dc92b1efbbd062f12cb598b6e0b778948
eugenia-hospitalcompare <eugenia@hospitalcompare.co.uk>
2019-07-08 09:59:14 +0100

    Finished main css layout

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,15 @@
 
 <footer class="mainFooter">
-    <small>&copy; <time id="thisYear"></time></small>
+    <nav class="footerCreds">
+        <ul class="credLinks">
+            <li><a href="">T&Cs</a></li>
+            <li><a href="">Accessibility</a></li>
+            <li><a href="">Private Policy</a></li>
+            <li><a href="">Cookie Policy</a></li>
+            <li><a href="">Cookie Policy</a></li>
+        </ul>
+        <small>&copy; <time id="thisYear"></time><a href="">Hospital Compare</a></small>
+    </nav>
 </footer>
 
 
