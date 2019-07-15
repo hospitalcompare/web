ee065d4d33fbee0def7d87106242fba6765829b0
eugenia-hospitalcompare <eugenia@hospitalcompare.co.uk>
2019-07-15 13:05:15 +0100

    Began creating searchresults page and re-usable components

---
+++ resources/views/components/basic/range.blade.php
@@ -0,0 +1,3 @@
+<label for="radiusProx">{{$label}}</label>
+<input type="range" id="radiusProx" name="radiusProx"
+       min="0" max="300" value="150">
--- resources/views/pages/resultspage.blade.php
+++ resources/views/pages/resultspage.blade.php
@@ -33,5 +33,66 @@
 
 @section('content')
 
+    <div class="filterParent">
+            <section class="filter">
+                <form class="formElem">
+                    <div class="postProx">
+                        <div class="postProxChild">
+                            @include('components.basic.textbox', ['placeholder' => 'Enter your postcode', 'inputClassName' => 'inputClass'])
+
+                        </div>
+                        <div class="postProxChild">
+                            <div class="rangeParent">
+                                @include('components.basic.range', ['label' => 'Within radius of', '' => ''])
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="selectProx">
+                        <div class="postProxChild">
+                            @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Choose your procedure'], ['id'=>2, 'name'=>'Choose your procedure']], 'selectClass' => 'searchPageSelect', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'Surgery Type', 'resultsLabel' => 'resultsLabel'])
+                            @include('components.basic.helplink', [ 'helpChar'=> '?', 'helpText' => 'surgery type...', 'className' => 'helpPosition1'])
+                        </div>
+                        <div class="postProxChild">
+                            @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Choose your procedure'], ['id'=>2, 'name'=>'Choose your procedure']], 'selectClass' => 'searchPageSelect', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'Waiting time', 'resultsLabel' => 'resultsLabel'])
+                            @include('components.basic.helplink', [ 'helpChar'=> '?', 'helpText' => 'waiting time...', 'className' => 'helpPosition2'])
+                        </div>
+                        <div class="postProxChild">
+                            @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Choose your procedure'], ['id'=>2, 'name'=>'Choose your procedure']], 'selectClass' => 'searchPageSelect', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'NHS choices user rating', 'resultsLabel' => 'resultsLabel'])
+                            @include('components.basic.helplink', [ 'helpChar'=> '?', 'helpText' => 'NHS choices...', 'className' => 'helpPosition3'])
+                        </div>
+                        <div class="postProxChild">
+                            @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Choose your procedure'], ['id'=>2, 'name'=>'Choose your procedure']], 'selectClass' => 'searchPageSelect', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'Care quality rating', 'resultsLabel' => 'resultsLabel'])
+                            @include('components.basic.helplink', [ 'helpChar'=> '?', 'helpText' => 'Care quality...', 'className' => 'helpPosition4'])
+                        </div>
+                        <div class="postProxChild">
+                            @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Choose your procedure'], ['id'=>2, 'name'=>'Choose your procedure']], 'selectClass' => 'searchPageSelect', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'NHS private/NHS funded', 'resultsLabel' => 'resultsLabel'])
+                            @include('components.basic.helplink', [ 'helpChar'=> '?', 'helpText' => 'NHS private...', 'className' => 'helpPosition5'])
+                        </div>
+                        <div class="postProxChild">
+                            @include('components.basic.button', ['classTitle' => 'greenOval blockDisplay', 'button' => 'Update results'])
+
+                        </div>
+                    </div>
+                </form>
+            </section>
+    </div>
+    <div class="sortParent">
+        <section class="sort">
+            <section class="sortBar">
+                    <div class="showSection">
+                        Showing ? out of ? provider(s)
+                    </div>
+                <div class="sortSection">
+                    @include('components.basic.selectbox', ['options' => [['id'=>1, 'name'=>'Distance from postcode'], ['id'=>2, 'name'=>'Distance from postcode']], 'selectClass' => 'sortBy', 'chevronFAClassName' => 'fa-chevron-down blackChevron', 'placeholder' => 'Sort by:', 'resultsLabel' => 'sortLabel'])
+
+                </div>
+            </section>
+        </section>
+        <section class="sortCategories">
+
+        </section>
+    </div>
 
 @endsection
