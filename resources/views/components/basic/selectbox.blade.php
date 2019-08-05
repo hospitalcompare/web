a9c24604bb942f3c7e6c862d592529eea91eb0f9
Tommy Eagle <tomeagle79@gmail.com>
2019-08-05 14:42:03 +0100

    Results page filters reworked.

--- resources/views/components/basic/selectbox.blade.php
+++ resources/views/components/basic/selectbox.blade.php
@@ -1,6 +1,8 @@
 <div class="selectParent  {{ empty($selectClassName) ? '' : $selectClassName }}">
     @if(!empty($showLabel))
-        <label class="_selectBox {{empty($resultsLabel) ? '' : $resultsLabel}}" for="{{empty($selectId)? '' : $selectId}}">{{empty($placeholder)? '': $placeholder}}</label>
+        <label class="_selectBox {{empty($resultsLabel) ? '' : $resultsLabel}}" for="{{empty($selectId)? '' : $selectId}}">
+            {{empty($placeholder)? '': $placeholder}}
+        </label>
     @endif
     <select class="{{empty($selectClass)? '' : $selectClass}}" id="{{empty($selectId)? '' : $selectId}}" name="{{$name}}">
         <option value="" disabled {{ empty(Request::input($name)) ? 'selected' : ''  }}>{{$placeholder}}</option>
