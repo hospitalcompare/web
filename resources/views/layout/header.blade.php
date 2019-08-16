7238a43ffd5b008e443c5aa7ebfc2d3ceffec1b0
eugenia-hospitalcompare <eugenia@hospitalcompare.co.uk>
2019-08-16 16:04:42 +0100

    Remove chevrons from header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,14 +7,18 @@
         <nav class="mainNav">
             <ul class="mainMenu">
                 <li><a href="">Our mission</a></li>
-                <li><a href="">Your Rights<i class="fas fa-chevron-down"></i></a>
+                <li><a href="">Your Rights
+{{--                        <i class="fas fa-chevron-down"></i>--}}
+                    </a>
 {{--                    <ul class="dropdownMenu">--}}
 {{--                        <li><a href="">Link text here</a></li>--}}
 {{--                        <li><a href="">Link text here</a></li>--}}
 {{--                        <li><a href="">Link text here</a></li>--}}
 {{--                    </ul>--}}
                 </li>
-                <li><a href="">How To Use<i class="fas fa-chevron-down"></i></a>
+                <li><a href="">How To Use
+{{--                        <i class="fas fa-chevron-down"></i>--}}
+                    </a>
 {{--                    <ul class="dropdownMenu">--}}
 {{--                        <li><a href="">Link text here</a></li>--}}
 {{--                        <li><a href="">Link text here</a></li>--}}
