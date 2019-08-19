388aa0b6e7a0be35b8911962d89acb6e492fa730
eugenia-hospitalcompare <eugenia@hospitalcompare.co.uk>
2019-08-19 11:39:45 +0100

    added favicon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,6 +8,10 @@
         <meta name='keywords' content='@yield('keywords')'>
         <meta name="viewport" content="@yield('mobile')">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/icons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/icons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/icons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('images/icons/site.webmanifest') }}">
     </head>
 
     <body class="@yield('body-class')">
