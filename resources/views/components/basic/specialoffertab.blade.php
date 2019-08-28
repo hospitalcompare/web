ab72c382f3e95802288003d2f1e68b5237b18424
Tommy Eagle <tomeagle79@gmail.com>
2019-08-28 15:40:13 +0100

    create component for offer tabs

---
+++ resources/views/components/basic/specialoffertab.blade.php
@@ -0,0 +1,33 @@
+{{--Special offers tabs in solutions bar --}}
+
+<div class="special-offer-tab">
+    <div class="special-offer-header d-flex justify-content-between align-items-center">
+        <div class="image-wrapper">
+            <img class="content" width="55" height="50" alt="Image of The Christie Main Site" src="{{ asset('../images/alder-1.png') }}">
+            <div class="nhs-hospital label"><p>NHS Hospital</p></div>
+        </div>
+        <div class="offer-text ml-4">
+            <div class="closed-text">
+                <p class="offer-title mb-0"><strong>NHS funded operation</strong></p>
+                <p class="offer-subtitle mb-0">At a private hospital of your choice</p>
+            </div>
+            <div class="open-text">
+                <p class="hospital-name mb-0"><strong>Spire Murrayfield</strong></p>
+                <p class="distance mb-0">35.3 miles away</p>
+            </div>
+        </div>
+        <span class="fa fa-chevron-up toggle-special-offer"></span>
+    </div>
+    <div class="special-offer-body">
+        <div class="bullets mb-4">
+            <ul>
+                <li>2 weeks</li>
+                <li>Outstanding</li>
+                <li>5 star user rating</li>
+            </ul>
+        </div>
+        <div class="btn-area text-right">
+            <a class="btn btn-icon btn-go">Enquire Now</a>
+        </div>
+    </div>
+</div>
--- resources/views/components/solutionsbar.blade.php
+++ resources/views/components/solutionsbar.blade.php
@@ -6,37 +6,9 @@
         </div>
         <ul class="solutions-menu d-flex m-0">
             <li class="d-block">
-                <div class="special-offer-tab">
-                    <div class="special-offer-header d-flex justify-content-between">
-                        <div class="image-wrapper">
-                            <img class="content" width="55" height="50" alt="Image of The Christie Main Site" src="{{ asset('../images/alder-1.png') }}">
-                            <div class="nhs-hospital label"><p>NHS Hospital</p></div>
-                        </div>
-                        <div class="offer-text ml-4">
-                            <div class="closed-text">
-                                <p class="offer-title mb-0"><strong>NHS funded operation</strong></p>
-                                <p class="offer-subtitle mb-0">At a private hospital of your choice</p>
-                            </div>
-                            <div class="open-text">
-                                <p class="hospital-name mb-0"><strong>Spire Murrayfield</strong></p>
-                                <p class="distance mb-0">35.3 miles away</p>
-                            </div>
-                        </div>
-                        <span class="fa fa-chevron-up toggle-special-offer"></span>
-                    </div>
-                    <div class="special-offer-body">
-                        <div class="bullets mb-4">
-                            <ul>
-                                <li>2 weeks</li>
-                                <li>Outstanding</li>
-                                <li>5 star user rating</li>
-                            </ul>
-                        </div>
-                        <div class="btn-area text-right">
-                            <a class="btn btn-icon btn-go">Enquire Now</a>
-                        </div>
-                    </div>
-                </div>
+                @include('components.basic.specialoffertab')
+            </li><li class="d-block">
+                @include('components.basic.specialoffertab')
             </li>
 {{--            <li class="d-block ml-3">--}}
 {{--                <a href="" class="btn btn-icon btn-special-tab">Special Offers</a>--}}
