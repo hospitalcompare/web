9d54175ed4dcb0ce9cc2e942a8abfaf83863138c
eugenia-hospitalcompare <eugenia@hospitalcompare.co.uk>
2019-08-29 14:32:53 +0100

    minor changes to special offer tabs

--- resources/views/components/basic/specialoffertab.blade.php
+++ resources/views/components/basic/specialoffertab.blade.php
@@ -1,14 +1,14 @@
 {{--Special offers tabs in solutions bar --}}
 
 <div class="special-offer-tab {{ $bgColor ?? '' }} ml-3">
-    <div class="special-offer-header d-flex justify-content-between align-items-center">
+    <div class="special-offer-header d-flex align-items-center">
         <div class="image-wrapper">
             <img class="content" width="55" height="50" alt="Image of The Christie Main Site" src="{{ asset('../images/alder-1.png') }}">
             <div class="nhs-hospital label">
 {{--                <p>NHS Hospital</p>--}}
             </div>
         </div>
-        <div class="offer-text ml-4">
+        <div class="offer-text">
             <div class="closed-text">
                 <p class="offer-title mb-0">{{ $headerText['closed']['title'] }}</p>
                 <p class="offer-subtitle mb-0">{{ $headerText['closed']['subtitle'] }}</p>
