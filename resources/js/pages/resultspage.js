8f351c4beb900940466ecc3c448803d11f2d78d3
HCLucian <lucian@hospitalcompare.co.uk>
2019-09-05 11:49:06 +0100

    - Adaptation of new star images with full/half/empty stars - Same method written in JQuery ( for the solutions bar )

--- resources/js/pages/resultspage.js
+++ resources/js/pages/resultspage.js
@@ -117,4 +117,53 @@ $(document).ready(function() {
     $(document).on("click", ".results-page .change-url", function(event) {
         window.location.href = updateQueryStringParam('hospital_type', 1);
     });
+
+    /**
+     * Generates HTML code based on a given rating ( between 0 - 5 )
+     * NB: Make sure that if this function is changed, the equivalent PhP function is changed as well ( /App/Helpers/Utils.php )
+     *
+     * @param rating
+     * @returns {string}
+     */
+    function getHtmlStars(rating) {
+        if(rating == null)
+            return "";
+
+        rating = parseFloat(rating);
+        if (rating > 5)
+            return "";
+
+        // Round down to get number of whole stars needed
+        var wholeStars = Math.floor(rating);
+
+        // This will be 1 if you have a half-rating, 0 if not.
+        var halfStar = Math.round(rating * 2) % 2;
+
+        // Get the number of empty stars
+        var emptyStars = 5 - wholeStars - halfStar;
+
+        // This will hold your html markup
+        var html = "";
+
+        // write img tags for each whole star
+        for (var i = 0; i < wholeStars; i++) {
+            html += "<img src='images/icons/star.svg' alt='Whole Star'>";
+        }
+
+        // write img tag for half star if needed
+        if (halfStar) {
+            html += "<img src='images/icons/half.svg' alt='Half Star'>";
+        }
+
+        //Check if we need to add empty stars as image
+        if(emptyStars != null && emptyStars > 0) {
+            for (var i = 0; i < emptyStars; i++) {
+               // html += "<img src='../images/icons/empty.svg' alt='Empty Star'>"; //TODO: Add the image for the empty stars
+                html += ""; //TODO: Remove this line and set the correct Empty Star above
+            }
+        }
+
+        return html;
+    }
+
 });
