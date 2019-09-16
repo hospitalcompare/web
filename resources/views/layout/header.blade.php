c501678d333f502e06034a7cb0bb3b2808a8093f
Tommy Eagle <tomeagle79@gmail.com>
2019-09-16 10:39:48 +0100

    Add home link to header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,27 +5,17 @@
             @include('components.logo', ['titleParent' => 'headerLogoParent', 'logoImg' => 'headerLogoImg', 'logoTitle' => 'headerLogoTitle'])
             <nav class="main-nav">
                 <ul class="main-menu">
+                    <li><a {{ Request::is( '/') ? 'class=active' : '' }} href="/">Home</a></li>
                     <li><a {{ Request::is( 'about-us') ? 'class=active' : '' }} href="/about-us">About Us</a></li>
                     <li><a {{ Request::is( 'your-rights') ? 'class=active' : '' }} href="/your-rights">Your Rights
                             {{--                        <i class="fas fa-chevron-down"></i>--}}
                         </a>
-                        {{--                    <ul class="dropdownMenu">--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                    </ul>--}}
                     </li>
                     <li><a {{ Request::is( 'how-to-use') ? 'class=active' : '' }} href="/how-to-use">How To Use
-                            {{--                        <i class="fas fa-chevron-down"></i>--}}
                         </a>
-                        {{--                    <ul class="dropdownMenu">--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                        <li><a href="">Link text here</a></li>--}}
-                        {{--                    </ul>--}}
                     </li>
                     <li><a {{ Request::is( 'faqs') ? 'class=active' : '' }} href="/faqs">FAQs</a></li>
-                    <li><a {{ Request::is( 'guides') ? 'class=active' : '' }} href="/guides">Guides</a></li>
+{{--                    <li><a {{ Request::is( 'guides') ? 'class=active' : '' }} href="/guides">Guides</a></li>--}}
                 </ul>
             </nav>
 
