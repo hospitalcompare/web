833cd26fa966cc1bf9939e9f5b3b2373c2cb5bda
Tommy Eagle <tomeagle79@gmail.com>
2019-09-18 12:57:07 +0100

    Range ticks now lining up with range steps

--- resources/views/components/basic/range.blade.php
+++ resources/views/components/basic/range.blade.php
@@ -2,7 +2,7 @@
 $vars = [0, 5, 10, 25, 50, 75, 100, 600];
 @endphp
 <div class="rangeParent range-slider">
-    <label for="radiusProx">{{$label}}</label>
+    <label for="radiusProx" class="d-none">{{$label}}</label>
     <input class="{{ $classTitle ?? '' }}"
            type="range"
            id="radiusProx"
@@ -11,7 +11,7 @@ $vars = [0, 5, 10, 25, 50, 75, 100, 600];
            max="{{$max ?? ''}}"
            value="{{$value ?? ''}}"
            step="{{$step ?? ''}}">
-    <div class="currentRadius range-slider__value"><span>{{ $vars[$value] }}</span> Miles</div>
+    <div class="currentRadius range-slider__value d-none"><span>{{ $vars[$value] }}</span> Miles</div>
     <ul class="range-labels">
         <li class="active selected">5</li>
         <li>10</li>
