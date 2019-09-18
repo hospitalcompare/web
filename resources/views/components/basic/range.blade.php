3dafd576e46b2ee02897a0a66b2cd20f83dd3f00
Tommy Eagle <tomeagle79@gmail.com>
2019-09-18 14:29:47 +0100

    new range slider with ticks complete

--- resources/views/components/basic/range.blade.php
+++ resources/views/components/basic/range.blade.php
@@ -2,7 +2,7 @@
 $vars = [0, 5, 10, 25, 50, 75, 100, 600];
 @endphp
 <div class="rangeParent range-slider">
-    <label for="radiusProx" class="d-none">{{$label}}</label>
+    <label for="radiusProx">{{$label}}</label>
     <input class="{{ $classTitle ?? '' }}"
            type="range"
            id="radiusProx"
@@ -11,9 +11,9 @@ $vars = [0, 5, 10, 25, 50, 75, 100, 600];
            max="{{$max ?? ''}}"
            value="{{$value ?? ''}}"
            step="{{$step ?? ''}}">
-    <div class="currentRadius range-slider__value d-none"><span>{{ $vars[$value] }}</span> Miles</div>
+    <div class="currentRadius range-slider__value"><span>{{ $vars[$value] }}</span> Miles</div>
     <ul class="range-labels">
-        <li class="active selected">5</li>
+        <li>5</li>
         <li>10</li>
         <li>25</li>
         <li>50</li>
