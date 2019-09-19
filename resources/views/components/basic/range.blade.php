a6a24018d0c8effe3aa27ed967b70df7b6cf0263
Tommy Eagle <tomeagle79@gmail.com>
2019-09-19 11:22:18 +0100

    Range slider padding fix - using range handle width variable.

--- resources/views/components/basic/range.blade.php
+++ resources/views/components/basic/range.blade.php
@@ -1,21 +1,23 @@
 <div class="rangeParent range-slider">
     <label for="radiusProx">{{$label}}</label>
-    <input class="{{ $classTitle ?? '' }}"
-           type="range"
-           id="radiusProx"
-           name="{{$name}}"
-           min="{{$min ?? ''}}"
-           max="{{$max ?? ''}}"
-           value="{{$value ?? ''}}"
-           step="{{$step ?? ''}}">
-    <div class="currentRadius range-slider__value"><span>{{ array_key_exists($value, \App\Helpers\Utils::sliderRange) ? \App\Helpers\Utils::sliderRange[$value] : 50 }}</span> Miles</div>
-    <ul class="range-labels">
-        <li>5</li>
-        <li>10</li>
-        <li>25</li>
-        <li>50</li>
-        <li>75</li>
-        <li>100</li>
-        <li>600</li>
-    </ul>
+    <div class="range-wrapper position-relative">
+        <div class="currentRadius range-slider__value d-none"><span>{{ array_key_exists($value, \App\Helpers\Utils::sliderRange) ? \App\Helpers\Utils::sliderRange[$value] : 50 }}</span> Miles</div>
+        <input class="{{ $classTitle ?? '' }}"
+               type="range"
+               id="radiusProx"
+               name="{{$name}}"
+               min="{{$min ?? ''}}"
+               max="{{$max ?? ''}}"
+               value="{{$value ?? ''}}"
+               step="{{$step ?? ''}}">
+        <ul class="range-labels">
+            <li>5</li>
+            <li>10</li>
+            <li>25</li>
+            <li>50</li>
+            <li>75</li>
+            <li>100</li>
+            <li>600</li>
+        </ul>
+    </div>
 </div>
