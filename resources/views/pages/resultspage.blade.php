72d329680fdba729a4692c6a67f05275851e18b0
Tommy Eagle <tomeagle79@gmail.com>
2019-09-30 13:00:19 +0100

    Range slider alignment

--- resources/views/pages/resultspage.blade.php
+++ resources/views/pages/resultspage.blade.php
@@ -11,7 +11,7 @@
 @section('body-class', 'results-page')
 
 @section('content')
-    @include('pages.pagesections.resultspageform', ['displayBlock' => true])
+    @include('pages.pagesections.resultspageform')
     <div id="sort_categories_parent" class="sort-categories-parent">
         <div class="sort-categories-header container">
             <div class="sort-categories-section-1"></div>
