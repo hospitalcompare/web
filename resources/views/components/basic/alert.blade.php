12296ecb9be60e81467897e78abe2b9b636de923
Tommy Eagle <tomeagle79@gmail.com>
2019-10-01 15:24:21 +0100

    Testing form validation and error handling

--- resources/views/components/basic/alert.blade.php
+++ resources/views/components/basic/alert.blade.php
@@ -1,10 +1,16 @@
 {{--        Eror messages for form --}}
 <div class="container">
-    <div id="hc_alert" class="alert collapse alert-dismissible fade" role="alert">
+    <div id="hc_alert" class="alert collapse alert-dismissible fade {{ !empty($errors) ? 'alert-danger show' : '' }}" role="alert">
 
         <button type="button" class="close" data-hide="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
-        <div class="alert-text"></div>
+        <div class="alert-text">
+            @if(!empty($errors))
+                @foreach($errors[0] as $error)
+                    {{ $error }}
+                @endforeach
+            @endif
+        </div>
     </div>
 </div>
