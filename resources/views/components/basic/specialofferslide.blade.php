e2c1db7f8d7a9acf24adcbb8bd06faba6dd5609f
Tommy Eagle <tomeagle79@gmail.com>
2019-10-02 12:40:48 +0100

    Special offer slide - tweak title and copy

--- resources/views/components/basic/specialofferslide.blade.php
+++ resources/views/components/basic/specialofferslide.blade.php
@@ -5,8 +5,8 @@
                     aria-label="Close">
             </button>
             <div class="col-6">
-                <h3 class="text-uppercase">Special Offer</h3>
-                <p>Get your Knee operation done in under 2 weeks on our self-pay
+                <p class="special-offer-title text-uppercase">Special Offer</p>
+                <p class="special-offer-copy">Get your Knee operation done in under 2 weeks on our self-pay
                     programme for £8,999 (reduced from £10,500).</p>
             </div>
             <div class="col-3">
