771cbc09bd264d83a00bef35c5d71f190115a395
HCLucian <lucian@hospitalcompare.co.uk>
2019-10-10 15:05:58 +0100

    - HTML showing on the doctor

--- resources/views/components/doctor.blade.php
+++ resources/views/components/doctor.blade.php
@@ -1,4 +1,4 @@
-<div class="doctor" data-message="{!! $doctor !!}">
+<div class="doctor" data-message="{{ $doctor }}">
     <a tabindex="0" data-offset="30px, 40px"
        class="d-inline-block" id="doctor-popover">
         <img src="{{ asset('../images/dr-stevini.svg') }}" alt="Illustration of Dr Stevini">
