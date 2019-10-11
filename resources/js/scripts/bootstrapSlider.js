96c90018e0547c7933ea226add5dc69316ee666a
Tommy Eagle <tomeagle79@gmail.com>
2019-10-11 15:43:20 +0100

    Relayout slider when opening form.

--- resources/js/scripts/bootstrapSlider.js
+++ resources/js/scripts/bootstrapSlider.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+// Initialise the bootstrap slider
     $("#radiusProx").slider({
         ticks: [1, 2, 3, 4, 5, 6, 7],
         ticks_labels: ['5', '10', '25', '50', '75', '100', 'England'],
-        handle: 'square'
-        // ticks_snap_bounds: 30
+        handle: 'square',
+        value: 4
     });
-})
+});
