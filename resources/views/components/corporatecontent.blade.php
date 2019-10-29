2986f4bab084f646ad2c108e0ffea1937269a4ed
tom-hcomp <tom@hospitalcompare.co.uk>
2019-10-29 11:06:47 +0000

    Carousel added with correct aspect ratios.

--- resources/views/components/corporatecontent.blade.php
+++ resources/views/components/corporatecontent.blade.php
@@ -157,12 +157,59 @@
                                 </ul>
                             </div>
                         </div>
-
                     </div>
                     <div class="media-pane col-5">
                         <div class="row mb-5">
                             <div class="col-6">
-                                {{ $d['corporate_content'] }}
+                                {{ !empty($d['corporate_content']) ?? '' }}
+                                <!--Carousel Wrapper-->
+                                    <div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel" data-interval="false">
+                                        <!--Slides-->
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="carousel-item active">
+                                                <img class="d-block h-100 content" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg"
+                                                     alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block h-100 content" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg"
+                                                     alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block h-100 content" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"
+                                                     alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <!--/.Slides-->
+                                        <!--Controls-->
+                                        <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                        <!--/.Controls-->
+                                        <ol class="_carousel-indicators indicators row">
+                                            <li data-target="#carousel-thumb" data-slide-to="0" class="active col-3">
+                                                <div class="col-inner">
+                                                    <img class="d-block h-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg" class="img-fluid">
+                                                </div>
+                                            </li>
+                                            <li data-target="#carousel-thumb" data-slide-to="1" class="col-3">
+                                                <div class="col-inner">
+                                                    <img class="d-block h-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg" class="img-fluid">
+                                                </div>
+                                            </li>
+                                            <li data-target="#carousel-thumb" data-slide-to="2" class="col-3">
+                                                <div class="col-inner">
+                                                    <img class="d-block h-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg" class="img-fluid">
+                                                </div>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                    <!--/.Carousel Wrapper-->
+
                             </div>
                             <div class="col-6">
                                 <div class="video-wrapper position-relative">
@@ -206,10 +253,6 @@
                                            'target'            => 'blank',
                                            'button'            => 'Enquire now',
                                            'id'                => 'enquire_'.$id])
-                                        {{--                                <a href="" id="1" class="btn btn-icon btn-enquire-now enquiry mt-auto" role="button" data-toggle="modal" data-modal-text="This is the default text for an enquiry to a private hospital" data-hospital-title="Christchurch Family Medical Centre " data-target="#hc_modal_enquire_private" data-image="" data-address="">Enquire now--}}
-                                        {{--                                    <i class=""></i>--}}
-
-                                        {{--                                </a>--}}
                                     </div>
                                 @endif
                             </div>
