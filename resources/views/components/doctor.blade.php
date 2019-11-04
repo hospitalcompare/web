6dc54c77b2f3decf33aa59df3eae60a74db4c4d8
tom-hcomp <tom@hospitalcompare.co.uk>
2019-11-04 16:12:46 +0000

    Doctor S styled up (apart from let's go btn)

--- resources/views/components/doctor.blade.php
+++ resources/views/components/doctor.blade.php
@@ -1,6 +1,14 @@
 <div id="doctor">
-    <a tabindex="0" data-doctor-delay="{{ $delay }}" data-message="{{ $doctor }}" data-offset="30px, 40px"
-       class="d-inline-block" id="doctor-popover">
-        <img src="{{ asset('../images/dr-stevini.svg') }}" alt="Illustration of Dr Stevini">
-    </a>
+    <span tabindex="0"
+          data-doctor-delay="{{ $delay }}"
+          data-message="{{ $doctor }}"
+          data-offset="30px, 40px"
+          class="d-inline-block stretched-link"
+          id="doctor-popover">
+        <p class="font-18 SofiaPro-Medium">Need some help? We are here to help you find the best hospital.</p>
+        <span class="btn btn-go">Let's go</span>
+    </span>
+    <img src="{{ asset('../images/dr-stevini.svg') }}"
+         alt="Illustration of Dr Stevini"
+         class="position-absolute">
 </div>
