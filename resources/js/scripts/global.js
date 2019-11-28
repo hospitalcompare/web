330fda70efc4f8065d81f38e508cf35bf0c4c17d
tom-hcomp <tom@hospitalcompare.co.uk>
2019-11-28 17:13:25 +0000

    Further cleanup.

--- resources/js/scripts/global.js
+++ resources/js/scripts/global.js
@@ -300,13 +300,6 @@ window.disableButtons = function (modifier = 0) {
     $notSelected
         .addClass('disabled')
         .parent();
-    // if (compareCount + modifier === 5) {
-    //
-    //     // .prop('title', 'Sorry, you have reached the limit of hospitals to compare.')
-    //     // .attr('data-toggle', 'tooltip');
-    //     // enable tooltips
-    //     // $('[data-toggle="tooltip"]').tooltip();
-    // }
 };
 
 // Reenable buttons to allow to add to compare
