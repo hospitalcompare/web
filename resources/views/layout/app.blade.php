f75c25f9276bc4a3ac7a32ad89b5fc4693e521b9
HCLucian <lucian@hospitalcompare.co.uk>
2019-12-12 11:00:23 +0000

    - Added `noindex,nofollow` to the meta tag

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,7 +10,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- Block indexing bots apart from live site -->
         @if(env('APP_ENV') !== 'live')
-            <meta name="robots" content="noindex">
+            <meta name="robots" content="noindex,nofollow">
         @endif
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/icons/apple-touch-icon.png') }}">
