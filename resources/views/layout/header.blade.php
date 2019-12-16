f5201ab81dc76b03d66ddc16df2067daa63d6c09
tom-hcomp <tom@hospitalcompare.co.uk>
2019-12-16 14:31:19 +0000

    Add new styles to mobile menu.

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -11,28 +11,49 @@
                     @svg('chevron-right-grey', 'd-lg-none')
                 </li>
                 <li>
-                    <a {{ Request::is( 'how-to-use') ? 'class=active' : '' }} href="/how-to-use">How To Use</a>
+                    <a {{ Request::is( 'how-to-use') ? 'class=active' : '' }} href="/how-to-use">How To Use
+                        @svg('chevron-right-grey', 'd-lg-none')
+                    </a>
                 </li>
                 <li>
-                    <a {{ Request::is( 'about-us') ? 'class=active' : '' }} href="/about-us">About Us</a>
+                    <a {{ Request::is( 'about-us') ? 'class=active' : '' }} href="/about-us">About Us
+                        @svg('chevron-right-grey', 'd-lg-none')
+                    </a>
                 </li>
                 <li>
-                    <a {{ Request::is( 'your-rights') ? 'class=active' : '' }} href="/your-rights">Your Rights</a>
+                    <a {{ Request::is( 'your-rights') ? 'class=active' : '' }} href="/your-rights">Your Rights
+                        @svg('chevron-right-grey', 'd-lg-none')
+                    </a>
                 </li>
                 <li>
-                    <a {{ Request::is( 'blogs') ? 'class=active' : '' }} href="/blogs">Blog</a>
+                    <a {{ Request::is( 'faqs') ? 'class=active' : '' }} href="/faqs">FAQs
+                        @svg('chevron-right-grey', 'd-lg-none')
+                    </a>
                 </li>
                 <li>
-                    <a {{ Request::is( 'faqs') ? 'class=active' : '' }} href="/faqs">FAQs</a>
+                    <a {{ Request::is( 'blogs') ? 'class=active' : '' }} href="/blogs">Blog
+                        @svg('chevron-right-grey', 'd-lg-none')
+                    </a>
                 </li>
                 {{--                    <li><a {{ Request::is( 'guides') ? 'class=active' : '' }} href="/guides">Guides</a></li>--}}
                 @if(env('APP_ENV') === 'local')
-                    <li><a {{ Request::is( 'results-page') ? 'class=active' : '' }} href="/results-page">Results page</a></li>
-                    <li><a {{ Request::is( 'test-page') ? 'class=active' : '' }} href="/test-page">Test page</a></li>
+                    <li>
+                        <a {{ Request::is( 'results-page') ? 'class=active' : '' }} href="/results-page">Results
+                            page
+                            @svg('chevron-right-grey', 'd-lg-none')
+                        </a>
+                    </li>
+                    <li>
+                        <a {{ Request::is( 'test-page') ? 'class=active' : '' }} href="/test-page">Test page
+                            @svg('chevron-right-grey', 'd-lg-none')
+                        </a>
+                    </li>
                 @endif
             </ul>
         </nav>
-        <div id="menu_toggle" class="d-lg-none"><img width="20px" height="20px" src="{{URL::asset('/images/icons/bars-solid.svg')}}" alt="Menu icon"></div>
+        <div id="menu_toggle" class="d-lg-none"><img width="20px" height="20px"
+                                                     src="{{URL::asset('/images/icons/bars-solid.svg')}}"
+                                                     alt="Menu icon"></div>
     </div>
 </header>
 
