dc4d320dba7b1eda27598de46ae4af93a1d0213e
tom-hcomp <tom@hospitalcompare.co.uk>
2019-12-20 14:29:26 +0000

    Mobile enquiry form complete

--- resources/views/mobile/components/modals/modalenquireprivatemobile.blade.php
+++ resources/views/mobile/components/modals/modalenquireprivatemobile.blade.php
@@ -2,7 +2,7 @@
 
 <div style="" class="modal modal-enquire-form fade show" id="hc_modal_mobile_enquire_private" tabindex="-1" role="dialog"
      aria-labelledby="" aria-modal="true" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog m-3 mx-md-auto" role="document">
         <div class="modal-content">
             @include('mobile.components.privateenquiryformmobile', [
                 'procedures'    => $procedures,
@@ -13,7 +13,7 @@
                 'email'         => $email ?? '',
                 'postCode'      => $postCode ?? '',
                 'phone'         => $phone ?? ''])
-            <div class="modal-footer">
+            <div class="modal-footer m-0 p-3">
                 <button type="button" class="btn btn-squared btn-squared_slim w-100 btn-black text-center" data-dismiss="modal">Close</button>
             </div>
         </div>
