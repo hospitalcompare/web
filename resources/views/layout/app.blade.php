07290e9e6ea67d82aba5be1f083c0b430ab35e3e
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-01 17:41:22 +0000

    Add GTM scripts to app.blade - only for live site

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang='en'>
     <head>
+        @if(env('APP_ENV') == 'live')
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MJZJN5W');</script>
+            <!-- End Google Tag Manager -->
+        @endif
+
+
         <title>Hospital Compare - @yield('title')</title>
         <meta charset='utf-8'>
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -31,6 +42,12 @@
 
     </head>
     <body class="@yield('body-class')">
+        @if(env('APP_ENV') == 'live')
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJZJN5W"
+                              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+        @endif
         <div class="header-wrapper">
             @include('layout.header')
         </div>
