9e0ab96514e3422ccf267a95d25c750a5d67d22b
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-03 15:16:38 +0000

    Add dynamic bullet points to special offer tab

--- resources/views/mobile/components/modals/modalmobilespecialoffertab.blade.php
+++ resources/views/mobile/components/modals/modalmobilespecialoffertab.blade.php
@@ -1,12 +1,18 @@
 <div id="hc_modal_mobile_special_offer_tab" class="modal fade" tabindex="-1" role="dialog">
     <div class="modal-dialog m-0 d-flex flex-column justify-content-end h-100" role="document">
-        <div class="modal-content position-relative result-item-mobile">
+        <div class="modal-content position-relative result-item-mobile pt-5">
             <div class="item-tags position-absolute d-flex">
                 <div class="{{ $specialOffer['hospital_type_id'] === 1 ? 'bg-violet private-hospital' : 'bg-blue nhs-hospital' }} hospital-type pp-2 bg-blue position-relative d-inline-block">
                     <p class="px-3 m-0 font-12">{{ $specialOffer['hospital_type_id'] === 1 ? 'Private' : 'NHS' }}</p>
                 </div>
             </div>
-            <span class="position-absolute"></span>
+            @include('components.basic.button', [
+                'classTitle'        => 'btn btn-compare compare font-12 shadow-none position-absolute mt-2 mr-2',
+                'htmlButton'        => true,
+                'buttonText'        => 'Add to compare',
+                'hospitalType'      => 1,
+                'svg'               => 'heart-solid',
+                'id'                => $specialOffer['id']])
             <div class="modal-header flex-column align-items-center">
                 <h5 class="special-offer-title font-18 text-center w-100 mb-2">Lowest Waiting time</h5>
                 <p class="mb-2 SofiaPro-SemiBold font-16">{{ $specialOffer['name'] }}</p>
@@ -27,15 +33,15 @@
 {{--                        </div>--}}
                         <div>
                             <ul class="mr-0 mb-3 font-16 SofiaPro-Medium">
-                                <li class="pink-tick">2 weeks</li>
-                                <li class="pink-tick">Outstanding</li>
-                                <li class="pink-tick">5 star user rating</li>
+                                <li class="pink-tick">{{ number_format((float)$specialOffer['waiting_time'][0]['perc_waiting_weeks'], 1).' Weeks ' }}</li>
+                                <li class="pink-tick">{{ $specialOffer['rating']['latest_rating'] . ' CQC Rating' }}</li>
+                                <li class="pink-tick">{{ (!empty($specialOffer['rating']['avg_user_rating'])) ? $specialOffer['rating']['avg_user_rating'] . ' star NHS Choices user rating' : null }}</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="modal-cta m-0 p-3">
+            <div class="modal-cta m-0 p-3 pb-5">
                 <div class="row">
                     <div class="col-6">
                         <button type="button" class="btn btn-squared btn-squared_slim w-100 btn-black text-center"
