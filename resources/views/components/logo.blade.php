1dd3c57c6f1c0a0cdb8e682437cdb3253a131832
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-10 10:11:32 +0000

    Replace logo inline SVGs with img tags

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,8 +1,8 @@
 <div class="{{$titleParent}}">
     <a href="{{url('/')}}" class="">
-        @svg('logo-mobile', 'd-md-none logo-mobile')
-        @svg('logo-desktop', 'd-none d-md-block logo-desktop')
-{{--        <img class="{{$logoImg}} d-md-none" src="{{ asset('images/icons/logo-mobile.svg') }}" alt="Hospital Compare logo">--}}
-{{--        <img class="{{$logoImg}} d-none d-md-block" src="{{ asset('images/icons/logo-desktop.svg') }}" alt="Hospital Compare logo">--}}
+{{--        @svg('logo-mobile', 'd-md-none logo-mobile')--}}
+{{--        @svg('logo-desktop', 'd-none d-md-block logo-desktop')--}}
+        <img class="{{$logoImg}} d-lg-none" height="40px"  src="{{ asset('images/icons/logo-mobile.svg') }}" alt="Hospital Compare logo">
+        <img class="{{$logoImg}} d-none d-lg-block" height="34px" src="{{ asset('images/icons/logo-desktop.svg') }}" alt="Hospital Compare logo">
     </a>
 </div>
