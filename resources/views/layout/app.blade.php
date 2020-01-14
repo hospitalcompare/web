570d38bdb0c0e502c645c6a650627df5c1e01dea
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-14 17:08:58 +0000

    Homepage FAQs icons.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -79,8 +79,9 @@
                         }
                     },
                     "theme": "hc-cookie",
-                    // "position": "bottom-right",
-                    "position": "bottom",
+                    "position": "bottom-right",
+                    // "position": "top",
+                    // "static": true,
                     "content": {
                         "message": "We use cookies to provide a better service. By closing this window or continuing to use our website you agree with our use of cookies. <br><br>Find out how to manage cookies and view our policy",
                         "dismiss": "Close",
