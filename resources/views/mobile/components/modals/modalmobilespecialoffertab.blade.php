c30425b55894446d0ba523faa846333bfab30099
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-20 09:56:46 +0000

    Close SO tab when opening enquiry form

--- resources/views/mobile/components/modals/modalmobilespecialoffertab.blade.php
+++ resources/views/mobile/components/modals/modalmobilespecialoffertab.blade.php
@@ -47,6 +47,7 @@
                                     'classTitle'        => 'btn-nested-enquire btn btn-squared btn-enquire btn-squared_slim btn-blue text-center enquiry btn-enquiry font-12 w-100 d-flex justify-content-center align-items-center flex-row-reverse px-3',
                                     'svg'               => 'circle-check',
                                     'target'            => 'blank',
+                                    'dismiss'           => true,
                                     'buttonText'        => 'Make enquiry',
                                     'hospitalIds'       => $specialOffer['id'],
                                     'id'                => 'enquire_' . $specialOffer['id']])
