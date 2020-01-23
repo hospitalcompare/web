0fc1575733925ebefd8c5a797e7431065bba50ce
tom-hcomp <tom@hospitalcompare.co.uk>
2020-01-23 14:11:29 +0000

    Building out user email template

--- resources/views/components/emails/emailuser.blade.php
+++ resources/views/components/emails/emailuser.blade.php
@@ -1,9 +1,81 @@
 <style>
-
+    table {
+        font-family: arial;
+        background-color: #fff;
+    }
 </style>
 
-<table>
-    <tr>
-        <td>This is the user email!</td>
-    </tr>
-</table>
+<center class="bg-grey">
+    <table width="600" style="">
+        <tr>
+            <td height="25"></td>
+        </tr>
+        <tr>
+            <td width="200"></td>
+            <td width="200"><img width="200" src="/images/icons/logo-email.svg" alt="Hospital compare logo"></td>
+            <td width="200"></td>
+        </tr>
+        <tr>
+            <td height="25"></td>
+        </tr>
+        <tr>
+            <td width="200"></td>
+            <td width="200">
+                <p style="text-align: center; font-size: 26px; color: #037098">Thank You, John.</p>
+                <p style="text-align: center; color: #037098">We have recieved your enquiry for</p>
+                <p style="text-align: center; font-weight: bold; color: #037098">Hospital name here</p>
+            </td>
+            <td width="200"></td>
+        </tr>
+        <tr>
+            <td height="25"></td>
+        </tr>
+        <tr>
+            <td colspan="3"><img width="600" src="/images/placeholder.jpg"
+                                 alt="Man on sofa browsing hospital compare website"></td>
+        </tr>
+        <tr>
+            <td height="25"></td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+    <table width="600">
+        <tr>
+            <td width="25"></td>
+            <td>
+                <p style="font-size: 20px; font-weight: 600">Next steps</p>
+                <p style="color: #757575">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Cupiditate dolorum eligendi magni odit quo. Dicta dignissimos dolorum eos facilis id mollitia sit. A
+                    aperiam beatae consequatur consequuntur, cupiditate doloribus ducimus eius eos est, eveniet facere
+                    fuga itaque laborum nemo obcaecati officiis pariatur provident quis rem repellendus rerum veniam
+                    voluptate voluptatem.</p>
+            </td>
+            <td width="25"></td>
+        </tr>
+        <tr>
+            <td colspan="3" height="25"></td>
+        </tr>
+    </table>
+    <table width="600">
+        <tr>
+            <td width="25"></td>
+            <td colspan="2">
+                <p style="font-size: 20px; font-weight: 600">Your email</p>
+                <p style="color: #757575">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Cupiditate dolorum eligendi magni odit quo. Dicta dignissimos dolorum eos facilis id mollitia sit. A
+                    aperiam beatae consequatur consequuntur, cupiditate doloribus ducimus eius eos est, eveniet facere
+                    fuga itaque laborum nemo obcaecati officiis pariatur provident quis rem repellendus rerum veniam
+                    voluptate voluptatem.</p>
+            </td>
+            <td width="25"></td>
+        </tr>
+        <tr>
+            <td width="25"></td>
+            <td>Title</td>
+            <td>Mr</td>
+            <td width="25"></td>
+        </tr>
+    </table>
+</center>
