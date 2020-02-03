7089f66982f9b9dcb3e91503cd0522af174544ff
HCLucian <lucian@hospitalcompare.co.uk>
2020-02-03 17:37:21 +0000

    - Added phone numbers + private self pay column

--- database/migrations/2019_07_01_101238_db_init.php
+++ database/migrations/2019_07_01_101238_db_init.php
@@ -76,6 +76,10 @@ class DbInit extends Migration
                 $table->string('tel_number')->nullable();
                 $table->string('url')->nullable();
                 $table->string('email')->nullable();
+                $table->string('phone_number1')->nullable();
+                $table->string('phone_number2')->nullable();
+                $table->string('phone_number3')->nullable();
+                $table->boolean('private_self_pay')->default(0);
 //                $table->string('report_url')->nullable();
                 $table->string('status')->default("active");
                 $table->timestamps();
