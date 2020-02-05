543c3d8589912498c87d6d91d4b6148020c87f96
tom-hcomp <tom@hospitalcompare.co.uk>
2020-02-05 10:52:43 +0000

    Alert popup - imcrease font size and cross size.

--- resources/views/components/basic/alert.blade.php
+++ resources/views/components/basic/alert.blade.php
@@ -4,15 +4,17 @@
      class="alert collapse _alert-dismissible fade mb-0 {{ !empty($hc_errors) ? 'alert-danger show' : '' }}"
      role="alert">
     <div class="container">
-        <button type="button" class="close" data-hide="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        <div class="alert-text">
-            @if(!empty($hc_errors))
-                @foreach($hc_errors[0] as $error)
-                    {{ $error }}
-                @endforeach
-            @endif
+        <div class="wrapper font-18 d-flex justify-content-between align-items-center">
+            <div class="alert-text">
+                @if(!empty($hc_errors))
+                    @foreach($hc_errors[0] as $error)
+                        {{ $error }}
+                    @endforeach
+                @endif
+            </div>
+            <button type="button" class="close d-flex justify-content-center align-items-center" data-hide="alert" aria-label="Close">
+                @svg('times-black')
+            </button>
         </div>
     </div>
 </div>
