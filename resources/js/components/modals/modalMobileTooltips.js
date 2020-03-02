e53a54216e0ffa1ca59dc36226fc23fc03e01b33
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-02 10:59:32 +0000

    Fix bug with mobile filter tooltips

--- resources/js/components/modals/modalMobileTooltips.js
+++ resources/js/components/modals/modalMobileTooltips.js
@@ -1,9 +1,11 @@
 // Bootstrap modal
 function showMobileInfoModal(event) {
+    // Clear the existing text
+    $('.modal-body').html('');
     var $button = $(event.relatedTarget);// Button that triggered the modal
-    var $content = $button.data('modal-text');// Extract info from data-* attributes
+    var content = $button.data('modal-text');// Extract info from data-* attributes
     var modal = $(this);
-    modal.find('.modal-body').html($content);
+    modal.find('.modal-body').html(content);
 }
 
 // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
