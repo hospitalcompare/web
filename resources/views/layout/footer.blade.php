003c38cb952132b825380a6123d3e66bc31b30cd
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-02 12:04:08 +0000

    Footer tweak

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -25,10 +25,10 @@
                     </nav>
                     <nav>
                         <ul class="footer-upper__secondary-menu mb-0">
-                            <li class="d-inline-block"><a href="/terms-of-use">Terms of Use</a></li>
-                            <li class="d-inline-block"><a href="/privacy-policy">Privacy Policy</a></li>
-                            <li class="d-inline-block"><a href="/cookie-policy">Cookie Policy</a></li>
-                            <li class="d-inline-block"><a href="/attributions">Attributions</a></li>
+                            <li class="d-inline-block mb-3"><a href="/terms-of-use">Terms of Use</a></li>
+                            <li class="d-inline-block mb-3"><a href="/privacy-policy">Privacy Policy</a></li>
+                            <li class="d-inline-block mb-3"><a href="/cookie-policy">Cookie Policy</a></li>
+                            <li class="d-inline-block mb-3"><a href="/attributions">Attributions</a></li>
                         </ul>
                     </nav>
                 </div>
