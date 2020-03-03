f8be31cca1d6e5675a0bf88c2d2b924cb38c2baf
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-03 16:12:36 +0000

    Tweak webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,10 +12,10 @@ const webpack = require('webpack');
  | file for the application as well as bundling up all the JS files.
  |
  */
-// const devTool = (mix.inProduction()) ? "none" : "source-map";
+const devTool = (mix.inProduction()) ? "none" : "source-map";
 
 mix.webpackConfig({
-    devtool: 'source-map',
+    devtool: devTool,
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
@@ -24,16 +24,6 @@ mix.webpackConfig({
             Popper: "popper"
         })
     ],
-    // module: {
-    //     rules: [{
-    //         test: /\.js?$/,
-    //         exclude: /(bower_components)/,
-    //         use: [{
-    //             loader: 'babel-loader',
-    //             options: mix.config.babel()
-    //         }]
-    //     }]
-    // }
 });
 
 //Copy all the images that are used on the Web Application to the right folder ( public/images )
@@ -45,26 +35,23 @@ mix.copy('node_modules/stickybits/dist/stickybits.min.js', 'public/js/stickybits
 
 console.log('Production mode', mix.inProduction());
 
-// if (mix.inProduction()) { // Don't add sourcemaps
-//     mix
-//         .options({
-//             autoprefixer: false,
-//             postCss: [
-//                 require('autoprefixer')({ // Add prefixes to css rules and fix bugs with flexbox
-//                     remove: false,
-//                     // browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'ios 8', 'ios 9', 'android 4'],
-//                 }),
-//                 require('postcss-flexbugs-fixes')(),
-//             ],
-//             processCssUrls: false
-//         })
-//         .sass('resources/sass/app.scss', 'public/css')
-//         .js(['resources/js/app.js'], 'public/js');
-// } else { // If in dev mode, add sourcemaps
-//
-// }
-
-mix.options({
+if (mix.inProduction()) { // Don't add sourcemaps
+    mix
+        .options({
+            autoprefixer: false,
+            postCss: [
+                require('autoprefixer')({ // Add prefixes to css rules and fix bugs with flexbox
+                    remove: false,
+                    // browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'ios 8', 'ios 9', 'android 4'],
+                }),
+                require('postcss-flexbugs-fixes')(),
+            ],
+            processCssUrls: false
+        })
+        .sass('resources/sass/app.scss', 'public/css')
+        .js(['resources/js/app.js'], 'public/js');
+} else { // If in dev mode, add sourcemaps
+    mix.options({
         autoprefixer: false,
         postCss: [
             require('autoprefixer')({
@@ -75,11 +62,14 @@ mix.options({
         ],
         processCssUrls: false
     })
-    // .sourceMaps(false, 'source-map')
-    .sourceMaps()
-    .sass('resources/sass/app.scss', 'public/css')
-    .js(['resources/js/app.js'], 'public/js');
+        // .sourceMaps(false, 'source-map')
+        .sourceMaps()
+        .sass('resources/sass/app.scss', 'public/css')
+        .js(['resources/js/app.js'], 'public/js');
     // .version();
+}
+
+
 
 // Reload browser when something changes
 mix.browserSync({
