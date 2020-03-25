261439b904ae3494791497108565408ad17ab8ca
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-25 11:54:07 +0000

    Modal tweaks - margin around

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 const pathRes = require('path');
 const webpack = require('webpack');
+let productionSourceMaps = false;
 
 /*
  |--------------------------------------------------------------------------
@@ -12,10 +13,9 @@ const webpack = require('webpack');
  | file for the application as well as bundling up all the JS files.
  |
  */
-const devTool = (mix.inProduction()) ? "none" : "source-map";
 
 mix.webpackConfig({
-    devtool: devTool,
+    // devtool: "source-map",
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
@@ -33,42 +33,21 @@ mix.copyDirectory('resources/fonts', 'public/fonts');
 mix.copyDirectory('resources/downloads', 'public/downloads');
 mix.copy('node_modules/stickybits/dist/stickybits.min.js', 'public/js/stickybits.js');
 
-console.log('Production mode', mix.inProduction());
-
-if (mix.inProduction()) { // Don't add sourcemaps
-    mix
-        .options({
-            autoprefixer: false,
-            postCss: [
-                require('autoprefixer')({ // Add prefixes to css rules and fix bugs with flexbox
-                    remove: false,
-                    // browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'ios 8', 'ios 9', 'android 4'],
-                }),
-                require('postcss-flexbugs-fixes')(),
-            ],
-            processCssUrls: false
-        })
-        .sass('resources/sass/app.scss', 'public/css')
-        .js(['resources/js/app.js'], 'public/js');
-} else { // If in dev mode, add sourcemaps
-    mix.options({
-        autoprefixer: false,
-        postCss: [
-            require('autoprefixer')({
-                remove: false,
-                // browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'ios 8', 'ios 9', 'android 4'],
-            }),
-            require('postcss-flexbugs-fixes')(),
-        ],
-        processCssUrls: false
-    })
-        // .sourceMaps(false, 'source-map')
-        .sourceMaps()
-        .sass('resources/sass/app.scss', 'public/css')
-        .js(['resources/js/app.js'], 'public/js');
-    // .version();
-}
-
+mix.options({
+    autoprefixer: false,
+    postCss: [
+        require('autoprefixer')({
+            remove: false,
+            // browsers: ['last 2 version', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'ios 7', 'ios 8', 'ios 9', 'android 4'],
+        }),
+        require('postcss-flexbugs-fixes')(),
+    ],
+    processCssUrls: false
+})
+    .sass('resources/sass/app.scss', 'public/css')
+    .js(['resources/js/app.js'], 'public/js')
+    .sourceMaps(productionSourceMaps,"source-map")
+    .version();
 
 
 // Reload browser when something changes
@@ -85,5 +64,5 @@ mix.browserSync({
     port: 8080,
     ghostMode: false
 })
-//Disable notification sounds
+    //Disable notification sounds
     .disableSuccessNotifications();
