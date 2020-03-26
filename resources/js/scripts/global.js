c12e39117e9530526dfcc81f7a514b0b33f56b18
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-26 15:14:38 +0000

    Custom drag cursor to get round IE bug.

--- resources/js/scripts/global.js
+++ resources/js/scripts/global.js
@@ -430,35 +430,3 @@ $(document).ready(function () {
         showSpecialAlert();
     }
 });
-
-// Click and drag to scroll *********************************************************** //
-const slider = document.querySelector('.synergy-ad-list');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-    console.log('mousedown');
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-
-slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-
-slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return;  // stop the fn from running
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk;
-});
