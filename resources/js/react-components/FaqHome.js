34d51d3a1b428fe835401f536e91d580b2a5253e
tom-hcomp <tom@hospitalcompare.co.uk>
2020-03-27 22:44:21 +0000

    Highlighting of search results working

--- resources/js/react-components/FaqHome.js
+++ resources/js/react-components/FaqHome.js
@@ -43,32 +43,35 @@ class FaqHome extends Component {
         })
     };
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+    };
+
     render() {
         let {searchTerm} = this.state;
         let filteredFaqs = this.state.allFaqs.filter(
             (faq) => {
-                const regex = new RegExp(this.state.searchTerm, 'gi');
+                const regex = new RegExp(searchTerm, 'gi');
                 return faq.question.match(regex) || faq.answer.match(regex);
             }
         );
+
         return (
             <div className="container">
-                <form className="d-flex mb-2">
+                <form onSubmit={this.handleSubmit}
+                      className="d-flex mb-2">
                     <div className="input-wrapper w-100">
                         <input id="faq_search_input"
                                className="w-100 form-control"
                                value={this.state.searchTerm}
                                placeholder="Search for a term"
                                onChange={this.handleChange}
-                               type="text" />
+                               type="text"/>
                     </div>
                 </form>
-                <div className="accordion">
+                <div className="accordion" id="faqs_accordion">
                     {
-                        filteredFaqs.map(
-                            (faq) => <FaqItem faq={faq}
-                                              key={faq.id} />
-                        )
+                        (filteredFaqs.length) ? filteredFaqs.map((faq) => <FaqItem searchTerm={this.state.searchTerm} faq={faq} key={faq.id}/>) : 'No faqs hombre'
                     }
                 </div>
             </div>
--- resources/js/react-components/FaqItem.js
+++ resources/js/react-components/FaqItem.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import '../scripts/vendor/highlight';
 
 class FaqItem extends Component {
     render() {
         const { question, answer, id } = this.props.faq;
+        const searchTerm = this.props.searchTerm;
+        const regex = new RegExp(searchTerm, 'gi');
+
         return (
             <div className="card">
                 <div className="card-header" id={`heading${id}`}>
@@ -10,13 +14,14 @@ class FaqItem extends Component {
                         <button className="btn btn-link collapsed text-left" type="button"
                                 data-toggle="collapse"
                                 data-target={`#collapse${id}`} aria-expanded="false"
-                                aria-controls={`collapse${id}`}>
-                            { question }
+                                aria-controls={`collapse${id}`}
+                                dangerouslySetInnerHTML={{ __html: question.replace(regex, `<span class="hl">${searchTerm}</span>`) }}>
+
                         </button>
                     </h2>
                 </div>
 
-                <div id={`collapse${id}`} className="_collapse" aria-labelledby={`heading${id}`}
+                <div id={`collapse${id}`} className="collapse" aria-labelledby={`heading${id}`}
                      data-parent="#faqs_accordion">
                     <div className="card-body"
                          dangerouslySetInnerHTML={{ __html: answer }}>
