ee5afa1d59da2b6ed4bee7fdcf23d463e01f2ade
tom-hcomp <tom@hospitalcompare.co.uk>
2020-04-01 10:57:38 +0100

    Clean up with map icon

--- resources/js/components/compare.js
+++ resources/js/components/compare.js
@@ -1,16 +1,12 @@
 //Check if we don't have the cookie and set it to 0
+import svgMapIcon from '../../images/icons/icon-map.svg';
+
 var $compareBar = $('.compare-hospitals-bar');
 var $compareContent = $('.compare-hospitals-content');
 var $compareButtonTitle = $('#open_shortlist');
 var $countSpan = $('#compare_number');
 var $heartIcon = $('#compare_heart');
-var $svgMapIcon =
-    `<svg class="map-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.37 49.61">
-        <g id="Layer_1" data-name="Layer 1">
-            <path fill="#037098" d="M26.38 34.66c4.77-8.54 7-14.25 7-18A16.69 16.69 0 000 16.68c0 3.73 2.22 9.44 7 18 2.9 5.2 5.81 9.72 6.63 11l2.37 3.6a.82.82 0 001.33 0l2.36-3.58c.37-.54 3.52-5.36 6.69-11.04zm-9.7 12.75L15 44.83s-3.28-5-6.61-10.93C3.86 25.82 1.58 20 1.58 16.68a15.11 15.11 0 0130.21 0c0 3.35-2.28 9.14-6.79 17.22-2.93 5.24-5.78 9.66-6.59 10.89z"/>
-            <path fill="#037098" d="M16.68 12.56a4 4 0 104 4 4 4 0 00-4-4z"/>
-        </g>
-    </svg>`;
+var $svgMapIcon = svgMapIcon;
 // Where we hold the counts of hospital types
 // No of each type of hospital in compare
 var nhsCountHolder = $('#nhs-hospital-count');
@@ -267,7 +263,7 @@ window.addHospitalToCompare = function (element) {
                 <div class="card-header p-0 pb-2 bg-white" id="heading${element.id}">
                      <button class="btn btn-link collapsed text-decoration-none p-0 rounded-0" data-toggle="collapse" data-target="#collapse${element.id}" aria-expanded="true" aria-controls="collapse${element.id}">
                          <p class="font-18 SofiaPro-SemiBold mb-2">${textTruncate(element.display_name, 30, '...')}</p>
-                         <p class="col-grey mb-2">${$svgMapIcon}${element.address.city} | ${hospitalType}</p>
+                         <p class="col-grey mb-2"><img class="map-icon" src="/images/icons/icon-map.svg" alt="Map icon">${element.address.city} | ${hospitalType}</p>
                          <p class="mb-2">${latestRating}&nbsp;|&nbsp;${getHtmlDashTickValue(waitingTime, " Weeks Average Waiting")}</p>
                      </button>
                      <div class="btn-area d-flex align-items-center">
