1afa843e6b23981726316a99219c46eb97d44bfa
tom-hcomp <tom@hospitalcompare.co.uk>
2020-04-24 11:16:00 +0100

    Add menu toggle inside React

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {BrowserRouter as Router, Redirect, Route, Switch, NavLink, Link} from 'react-router-dom'
 import HomePage from './react/pages/HomePage'
 import FaqHome from './react/pages/FaqHome'
 import Header from './react/components/Header'
 import Footer from './react/components/Footer'
 
-const App = () => {
-    return (
-        <React.Fragment>
-            <Header/>
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route exact path="/faqs" component={FaqHome}/>
-                    <Redirect to="/"/>
-                </Switch>
-            </Router>
-            <Footer/>
-        </React.Fragment>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+
+    handleMenuToggle = () => {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        })
+
+        this.state.menuOpen
+            ? document.body.classList.add('menu-open')
+            : document.body.classList.remove('menu-open');
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Header handleMenuToggle={this.handleMenuToggle}/>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route exact path="/faqs" component={FaqHome}/>
+                        <Redirect to="/"/>
+                    </Switch>
+                </Router>
+                <Footer/>
+            </React.Fragment>
+        );
+    }
 };
 
 export default App
