19a248c1f8b7a58293584fddd83f65544c32cce0
tom-hcomp <tom@hospitalcompare.co.uk>
2020-05-07 12:23:42 +0100

    Move postcode validation to global script.

--- resources/js/react/components/forms/SearchForm.js
+++ resources/js/react/components/forms/SearchForm.js
@@ -3,21 +3,16 @@ import SelectComponent from '../basic/SelectComponent';
 import Form from "react-bootstrap/Form";
 import {withRouter} from "react-router";
 import axios from "axios";
-
-// Check valid postcode
-function valid_postcode(postcode) {
-    postcode = postcode.replace(/\s/g, "");
-    var regex = /^((([A-PR-UWYZ][0-9])|([A-PR-UWYZ][0-9][0-9])|([A-PR-UWYZ][A-HK-Y][0-9])|([A-PR-UWYZ][A-HK-Y][0-9][0-9])|([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))\s?([0-9][ABD-HJLNP-UW-Z]{2})|(GIR)\s?(0AA))$/i;
-    return regex.test(postcode);
-}
+import '../../scripts/validatePostcode';
 
 class SearchForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             radii: [],
             procedure: '0',
             postcode: '',
+            fakePostcode: '',
             returnedPostcodes: [],
             radius: 50,
             haveResults: false,
@@ -27,10 +22,6 @@ class SearchForm extends Component {
 
     componentDidMount() {
         const apiUrl = `api/getRadius`;
-        // const bodyparams = {
-        //     dataType: "json",
-        //     contentType: "application/json; charset=utf-8"
-        // };
         const config = {
             headers: {
                 Authorization: 'Bearer mBu7IB6nuxh8RVzJ61f4'
@@ -85,7 +76,13 @@ class SearchForm extends Component {
 
     handleClick = (e) => {
         e.preventDefault();
-        const {procedure, postcode, radius} = this.state;
+        const {procedure, postcode, radius, fakePostcode} = this.state;
+        if(fakePostcode !== "") {
+            alert('Fuck off robot!');
+            // TODO: create a function to display a message saying 'something went wrong'
+            return;
+        }
+
         this.props.history.push({
             pathname: '/results-page/',
             search: `?postcode=${postcode}&procedure=${procedure}&radius=${radius}`
@@ -118,7 +115,7 @@ class SearchForm extends Component {
     };
 
     render() {
-        const {returnedPostcodes, postcode, haveResults, showPostcodes, procedure} = this.state;
+        const {returnedPostcodes, postcode, haveResults, showPostcodes, procedure, fakePostcode} = this.state;
 
         return (
             <React.Fragment>
@@ -130,13 +127,15 @@ class SearchForm extends Component {
                                          handleChange={this.handleSelect}/>
                     </div>
                     <div className="form-child postcode-parent position-relative">
-                        {/*<label for"fake_postcode" className="d-none">*/}
-                        {/*    <span className="sr-only">fake postcode</span>*/}
-                        {/*</label>*/}
-                        <input name="fake_postcode"
-                               id="fake_postcode"
+                        <label htmlFor="fakePostcode" className="d-none">
+                            <span className="sr-only">fake postcode</span>
+                        </label>
+                        <input name="fakePostcode"
+                               id="fakePostcode"
+                               value={fakePostcode}
                                type="text"
-                               style={{display: 'none'}}/>
+                               style={{display: 'none'}}
+                               onChange={this.handleChange}/>
 
                         <div className="input-wrapper position-relative">
                             <input type="text"
@@ -168,8 +167,9 @@ class SearchForm extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className={`form-child radius-parent full-left column-layout ${postcode.length > 0 ? 'revealed-down' : ''} position-relative`}
-                         data-reveal-direction="down">
+                    <div
+                        className={`form-child radius-parent full-left column-layout ${postcode.length > 0 ? 'revealed-down' : ''} position-relative`}
+                        data-reveal-direction="down">
                         <select
                             className="w-100 distance-dropdown big select-picker"
                             onChange={this.handleChange}
@@ -189,12 +189,13 @@ class SearchForm extends Component {
                     </div>
                     <button onClick={this.handleClick}
                             id="submit_search"
-                            className="btn btn-squared btn-block text-center btn-brand-primary-1 mb-3 font-18">Search
-                        now
+                            className="btn btn-squared btn-block text-center btn-brand-primary-1 mb-3 font-18">
+                        Search now
                     </button>
                     <div className='browse-button text-left text-lg-center'>
                         <a href="/results-page"
-                           className="col-grey pl-0 btn-plain btn-icon-arrow w-100 d-flex align-items-center justify-content-lg-center">Browse all hospitals</a>
+                           className="col-grey pl-0 btn-plain btn-icon-arrow w-100 d-flex align-items-center justify-content-lg-center">Browse
+                            all hospitals</a>
                     </div>
                 </Form>
             </React.Fragment>
---
+++ resources/js/react/scripts/validatePostcode.js
@@ -0,0 +1,7 @@
+
+// Check valid postcode
+function valid_postcode(postcode) {
+    postcode = postcode.replace(/\s/g, "");
+    var regex = /^((([A-PR-UWYZ][0-9])|([A-PR-UWYZ][0-9][0-9])|([A-PR-UWYZ][A-HK-Y][0-9])|([A-PR-UWYZ][A-HK-Y][0-9][0-9])|([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))\s?([0-9][ABD-HJLNP-UW-Z]{2})|(GIR)\s?(0AA))$/i;
+    return regex.test(postcode);
+}
